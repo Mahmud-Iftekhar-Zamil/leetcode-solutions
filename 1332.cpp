2496b0ecf1489a417b4f487bd5cd266c84dd6600
Mahmud Iftekhar Zamil <56012002+Mahmud-Iftekhar-Zamil@users.noreply.github.com>
2021-10-14 22:24:03 +0600

    Create 1332.cpp

---
+++ 1332.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if(s.length()==1) return 1;
+        string rev = s;
+        reverse(rev.begin(),rev.end());
+        if(s==rev) return 1;
+        else return 2;
+    }
+};
